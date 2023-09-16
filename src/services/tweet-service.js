91f8a9154aa380f6980f7fc330df3aba1d0b7c58
Shubham <shubhampandey0806@gmail.com>
2023-09-16 16:50:08 +0530

    added index to hashtag and removed hashtag array from tweet schema

--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -9,7 +9,9 @@ class TweetService {
 
     async create(data) {
         const content = data.content;
-        const tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag) => tag.substring(1)); // this regex extracts hashtags
+        const tags = content.match(/#[a-zA-Z0-9_]+/g)
+        .map((tag) => tag.substring(1))
+        .map(tag => tag.toLowerCase()); // this regex extracts hashtag
         const tweet = await this.tweetRepository.create(data);
         let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
         let titleOfPresenttags = alreadyPresentTags.map(tags => tags.title);
