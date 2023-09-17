57ae2a9be591f7273962f72fb134e6c197d19588
Shubham <shubhampandey0806@gmail.com>
2023-09-17 11:20:14 +0530

    Updated comment model with refPath

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,17 +1,26 @@
 //const mongoose = require('mongoose');
 import mongoose from "mongoose";
 import mongoose from "mongoose";
-const hashtagSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    }, 
-    tweets: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Tweet'
-        }
-    ]
+const commentSchema = new mongoose.Schema({
+    content :{
+        type : String ,
+        required : true
+    },
+    userId : {
+        Type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required : true
+    },
+    onModel :{
+        type: String ,
+        required : true,
+        enum :['Tweet','Comment']
+    },
+    commentable :{
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+        refPath : 'onModel'
+    }
 }, {timestamps: true});
 export default Hashtag;
  
